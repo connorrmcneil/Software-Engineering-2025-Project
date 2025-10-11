ce356bc028a42e0523d8e7a80d8e3f85e96aee82
Alison Cook <107211864+cookAlison@users.noreply.github.com>
2025-10-11 13:21:08 -0300

    SCRUM-50 added button from Tango app

--- packages/front-end/src/pages/game1.tsx
+++ packages/front-end/src/pages/game1.tsx
@@ -1,3 +1,10 @@
+import {Button} from '@/assets/Components/Button'
+
 export function Game1(){
-    return <div>Game1's page!</div>
+    return (
+        <>
+            <div>Game1's page!</div>
+            <Button>Play (invokes character selection)</Button>
+        </>
+    );
 }
