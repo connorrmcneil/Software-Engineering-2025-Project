c1bd508fcd7c6fcba2336ea40fd3bde463762714
JackGreenlaw <162923396+JackGreenlaw@users.noreply.github.com>
2025-12-03 12:38:19 -0400

    Minor fix of footer links

--- packages/front-end/src/components/Footer.tsx
+++ packages/front-end/src/components/Footer.tsx
@@ -1,31 +1,28 @@
-import { Anchor, Container, Group } from '@mantine/core';
-import classes from '@/styles/FooterStyle.module.css';
+import {Anchor, Container, Group} from '@mantine/core'
+import {Link} from 'react-router'
+
+import classes from '@/styles/FooterStyle.module.css'
+
 const links = [
   {link: '/faq', label: 'FAQ'},
   {link: '/privacypolicy', label: 'Privacy'}
 ]
 
 export function Footer() {
-    const items = links.map((link) => (
-      <Anchor<'a'>
-        c="dimmed"
-        key={link.label}
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-        size="sm"
-      >
-        {link.label}
-      </Anchor>
-    ));
-  
-    return (
-      <div className={classes.footer}>
-        <Container className={classes.inner}>
-            <Group className={classes.angie}>
-                <span>Mikwite'tmk+t Angie</span>
-            </Group>
-            <Group className={classes.links}>{items}</Group>
-        </Container>
-      </div>
-    );
-  }
+  const items = links.map(link => (
+    <Anchor<typeof Link> component={Link} to={link.link} c="dimmed" key={link.label} size="sm">
+      {link.label}
+    </Anchor>
+  ))
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Group className={classes.angie}>
+          <span>Mikwite'tmk+t Angie</span>
+        </Group>
+        <Group className={classes.links}>{items}</Group>
+      </Container>
+    </div>
+  )
+}
